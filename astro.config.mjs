7e7601b9ed7a2b55fb31a662fe8b13b95e922acf
Stefan le Roux <stefan@vivtec.co.za>
2026-06-23 20:43:04 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()]
+	site: "https://vivtec-net.github.io",
+	base: "/inkfish.co.za/",
+	trailingSlash: "always",
+	integrations: [mdx(), sitemap()],
 });
